885484af2e0f19b8297c77ae684995817e0b2964
Михаил Поздняков <154692665+m1xach@users.noreply.github.com>
2025-10-07 18:25:30 +0300

    Add files via upload

---
+++ dz4/Source.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+    int A, B;
+    printf("Введите пароль A и B: ");
+    scanf ("%d %d", &A, &B);
+    if ((A % 2 == 0 && B % 2 != 0) || (A % 2 != 0 && B % 2 == 0)) 
+    {
+        printf("Пароль активен\n");
+    }
+    else 
+    {
+        printf("Пароль неактивен\n");
+    }
+
+    return 0;
+}
